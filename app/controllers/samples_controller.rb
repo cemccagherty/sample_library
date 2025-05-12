c2e3c026bb47c0ca77844a28882b4fee406d697a
Christian McCagherty <christianmccagherty164@gmail.com>
2025-05-12 16:49:40 +0100

    added new and create actions for sample

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,4 +2,24 @@ class SamplesController < ApplicationController
   def index
     @samples = Sample.where(user: current_user)
   end
+
+  def new
+    @sample = Sample.new
+  end
+
+  def create
+    @sample = Sample.create(sample_params)
+    @sample.user = current_user
+    if @sample.save
+      redirect_to samples_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sample_params
+    params.require(:sample).permit(:name, :bpm, :key, :source, :audio_file)
+  end
 end
