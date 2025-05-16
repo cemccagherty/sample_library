c8a749a1443ed27672e28808af93712b4d491b85
Christian McCagherty <christianmccagherty164@gmail.com>
2025-05-16 12:40:35 +0100

    added sample validations

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,4 +1,25 @@
 class Sample < ApplicationRecord
   belongs_to :user
   has_one_attached :audio_file
+
+  validates :name, presence: true
+  validates :name, length: { in: 1..30 }
+
+  validates :bpm, inclusion: { in: 20..300 }, allow_nil: true
+  # allow_nil: true to accomodate N/A bpm input for ambient etc
+
+  validates :source, length: { in: 1..150 }
+
+  validates :audio_file, attached: true
+  validates :audio_file, content_type: [
+    'audio/mpeg',
+    'audio/x-aiff',
+    'audio/vnd.wave',
+    'audio/x-wav',
+    'audio/x-flac'
+  ]
+  # audio/x-wav included for compatability with legacy WAV identifying format.
+  validates :audio_file, duration: { less_than_or_equal_to: 5.minutes }
+  validates :audio_file, size: { less_than: 50.megabytes }
+  # large audio_file size to accomodate wav uploads.
 end
